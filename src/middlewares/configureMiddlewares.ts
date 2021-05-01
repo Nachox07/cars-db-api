c5084f7c5322e4162368e07451e04e24816daee8
Nacho Gonzalez-Garilleti <jos.ruiz@williamhill.com>
2021-05-01 13:29:51 +0200

    feat: integrate helmet for extra security

--- src/middlewares/configureMiddlewares.ts
+++ src/middlewares/configureMiddlewares.ts
@@ -1,4 +1,5 @@
 import express, { Application } from 'express';
+import helmet from 'helmet';
 import expressPino from 'express-pino-logger';
 import logger from '../logger';
 import authorizationHandler from './authorizationHandler';
@@ -6,6 +7,7 @@ import authorizationHandler from './authorizationHandler';
 const configureMiddlewares = (app: Application) => {
   logger.info('configuring middlewares');
 
+  app.use(helmet());
   app.use(express.json());
   app.use(
     expressPino({
